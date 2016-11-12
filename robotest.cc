fddb76efa16c9b3e95de79280dcfd939ece9336c
Shegufta B Ahsan <plaban777@gmail.com>
2016-11-12 16:13:52 -0600

    push using shell script

--- robotest.cc
+++ robotest.cc
@@ -591,6 +591,9 @@ void navigate(void* _robot)
                         }
                         else if (rotationLimiter < NS_SURVEY_SLOT_MAX)
                         {
+                            cout <<"\t #NS_SURVEY :: surveyManagerPtr->getSignalStrength(wallSignal) = "<<surveyManagerPtr->getSignalStrength(wallSignal)<<"   | wallSignal = "<<wallSignal<<endl;
+
+
                             rotationLimiter++; // we will not count the backup time here !
 
                             if (NULL == surveyManagerPtr)
