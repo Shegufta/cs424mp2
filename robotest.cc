48519a4d6d350cc61c6df3978c1de25c1df0eb72
Shegufta B Ahsan <plaban777@gmail.com>
2016-11-14 01:01:39 -0600

    seems like this follow wall is more or less working

--- robotest.cc
+++ robotest.cc
@@ -364,7 +364,7 @@ void navigate(void* _robot)
     const int n_SEARCHING_STRAIGHT_SPEED = 200;
     const int n_SEARCH_FRONT_WALL_SLOW_BACKUP = 100; // if found front wall, then backup slowly
 
-    const int SEARCHING_SPEED = 100;
+    const int SEARCHING_SPEED = 100; 
     const int MID_BACKUP_DIST_mm = 10;
     const int SEARCH_R_WALL_ForwardDist_mm = 120;
     const int NS_SURVEY_SLOT_MAX = 70*2; // NOTE: at n_SEARCHING_ROTATION_SPEED = 300mmps and 15ms sleep interval, it takes approx 184 slot for a 360 degree rotation/// We will scan max 135 degree, henc our max slot will be approx 70....   Note, ((360/8)*3) = 135
